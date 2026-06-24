8bc7c2409af6620dd5b9c96bc50b58bd804ec86d
arashik65 <mdashikur6398+@gmail.com>
2026-06-25 05:12:04 +0600

    all card design topgeneration

---
+++ src/app/components/PhotoCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Image from "next/image";
+import { FaHeart } from 'react-icons/fa';
+import { BiDownload } from 'react-icons/bi';
+import { Button, Chip, Separator } from '@heroui/react';
+
+const PhotoCard = ({photo}) => {
+    console.log(photo);
+    return (
+      <card className='border rounded-xl'  >
+         <div className='relative w-full  aspect-square'>
+            <Image  
+            src={photo.imageUrl}
+            fill={true}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            alt={photo.title}
+            className=' rounded-xl'
+            />
+            <Chip size='sm' className='absolute right-2 top-2'>{photo.category}</Chip>
+         </div>
+         <div>
+            <h2 className='font-medium'>{photo.title}</h2>
+         </div>
+         
+         <div className='flex gap-5 '>
+            <div className='flex items-center gap-2'>
+            <p> <FaHeart /></p>
+            <p>{photo.likes}</p>
+         </div>
+
+         <Separator orientation='vertical'/>
+         <div className='flex items-center gap-2'>
+            <p> <BiDownload /></p>
+            <p>{photo.downloads}</p>
+         </div>
+         </div>
+         <Button variant='outline' className={'w-full'}>View</Button>
+      </card>
+    );
+};
+
+export default PhotoCard;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
+import TopGeneration from "./components/TopGeneration";
 
 export default function Home() {
   return (
     <div>
         <Banner></Banner>
+        <TopGeneration></TopGeneration>
     </div>
   );
 }
